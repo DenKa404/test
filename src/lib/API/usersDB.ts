7529465d1a32c39fe8ef12b2db92f475291b0463
Денис Кабайлов <denis2000kab@gmail.com>
2026-03-16 21:20:04 +0300

    Добавлено нове поле isFavorite

--- src/lib/API/usersDB.ts
+++ src/lib/API/usersDB.ts
@@ -2,6 +2,7 @@ interface UserData {
 	id: number;
 	name: string;
 	email: string;
+	isFavorite: boolean;
 }
 
 const users: UserData[] = [
@@ -9,21 +10,25 @@ const users: UserData[] = [
 		id: 1,
 		name: 'Petya Slavin',
 		email: 'petya@mail.ru',
+		isFavorite: true,
 	},
 	{
 		id: 2,
 		name: 'Sasha Petrovich',
 		email: 'sasha@ya.ru',
+		isFavorite: false,
 	},
 	{
 		id: 3,
 		name: 'Kolya Semenov',
 		email: 'Kolya@gmail.com',
+		isFavorite: false,
 	},
 	{
 		id: 4,
 		name: 'Semen Andreev',
 		email: 'semen@somedomen.net',
+		isFavorite: false,
 	},
 ];
 
