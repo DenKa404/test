adb514cc1c62390e67d8591a58389e5b5d0e6fa8
Денис Кабайлов <denis2000kab@gmail.com>
2026-03-16 23:08:53 +0300

    Добавлен Header

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -2,6 +2,7 @@ import SearchInput from '@/src/components/ui/searchInput';
 import { getUsers } from '@/src/lib/API/users';
 import UsersTable from './UsersTable';
 import Link from 'next/link';
+import Header from '@/src/app/users/header';
 
 interface UsersPageProps {
 	searchParams: {
@@ -15,14 +16,7 @@ async function UsersPage({ searchParams }: UsersPageProps) {
 
 	return (
 		<div className="flex flex-col items-center max-w-2xl mx-auto p-6 space-y-6">
-			<div className="w-full grid grid-cols-[100px_minmax(0,1fr)_100px] grid-flow-col">
-				<Link className="hover:text-sky-600 hover:underline text-center" href="/">
-					Домашняя страница
-				</Link>
-				<div className='flex justify-center items-center'>
-					<h1 className="text-2xl font-bold">Пользователи</h1>
-				</div>
-			</div>
+			<Header />
 			<SearchInput rootURL="users" queryParamName="searchSubString" />
 			<UsersTable users={usersFromServ} />
 		</div>
