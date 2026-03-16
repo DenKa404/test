ce53d75457f49b9abdd0ae6296c4e9216a426e53
Денис Кабайлов <denis2000kab@gmail.com>
2026-03-16 22:30:45 +0300

    Добавлена ссылка на домашнюю страницу

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,6 +1,7 @@
 import SearchInput from '@/src/components/ui/searchInput';
 import { getUsers } from '@/src/lib/API/users';
 import UsersTable from './UsersTable';
+import Link from 'next/link';
 
 interface UsersPageProps {
 	searchParams: {
@@ -14,9 +15,16 @@ async function UsersPage({ searchParams }: UsersPageProps) {
 
 	return (
 		<div className="flex flex-col items-center max-w-2xl mx-auto p-6 space-y-6">
-			<h1 className="text-2xl font-bold">Пользователи</h1>
+			<div className="w-full grid grid-cols-[100px_minmax(0,1fr)_100px] grid-flow-col">
+				<Link className="hover:text-sky-600 hover:underline text-center" href="/">
+					Домашняя страница
+				</Link>
+				<div className='flex justify-center items-center'>
+					<h1 className="text-2xl font-bold">Пользователи</h1>
+				</div>
+			</div>
 			<SearchInput rootURL="users" queryParamName="searchSubString" />
-			<UsersTable users={usersFromServ}/>
+			<UsersTable users={usersFromServ} />
 		</div>
 	);
 }
