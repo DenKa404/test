2a2bc5159f2b7609bbe1cff045aa797ff6671ca0
Денис Кабайлов <denis2000kab@gmail.com>
2026-03-16 22:30:16 +0300

    Упрощена кнопка

--- src/components/ui/favouriteButton.tsx
+++ src/components/ui/favouriteButton.tsx
@@ -2,26 +2,20 @@
 
 import { useState } from 'react';
 import { Star } from 'lucide-react';
-import { cn } from '@/src/lib/utils';
 
 export default function FavoriteButton({
-	className,
 	isFavorite,
-	onClick,
-	...props
-}: React.HTMLAttributes<HTMLDivElement> & { isFavorite: boolean }) {
+}: {
+	isFavorite: boolean;
+}) {
 	const [favorite, setFavorite] = useState(isFavorite);
 
 	return (
 		<div
-			className={"size-fit items-center cursor-pointer"}
-			onClick={(e) => {
-				setFavorite(!favorite);
-				onClick && onClick(e);
-			}}
-			{...props}
+			className={'size-fit items-center cursor-pointer'}
+			onClick={() => setFavorite(!favorite)}
 		>
-			{favorite ? <Star className={cn(className, "fill-chart-1")} /> : <Star />}
+			{favorite ? <Star className="fill-chart-1" /> : <Star />}
 		</div>
 	);
 }
