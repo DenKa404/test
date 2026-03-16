2cb276a18cf7eafc4890f0fd90ec2975395fe9d5
Денис Кабайлов <denis2000kab@gmail.com>
2026-03-16 18:56:51 +0300

    Форматированы строки

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>Список пользователей</h1>
-    </div>
-  );
+	return (
+		<div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+			<h1>Список пользователей</h1>
+		</div>
+	);
 }
