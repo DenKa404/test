7c04022fde7500148e68e63ae6003aa08b8cff0e
Денис Кабайлов <denis2000kab@gmail.com>
2026-03-16 22:30:05 +0300

    Добавлено форматирование

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,16 @@ export default function Home() {
 	return (
 		<div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
 			<div className="flex flex-col h-full justify-between">
-				<h1 className='shrink-0 font-semibold text-4xl h-fit'>Начальная страница</h1>
-				<div className='flex-1 h-full text-center'>
-					<Link className='hover:text-sky-600 hover:underline ' href="/users">Открыть список пользователей</Link>
+				<h1 className="shrink-0 font-semibold text-4xl h-fit">
+					Начальная страница
+				</h1>
+				<div className="flex-1 h-full text-center">
+					<Link
+						className="hover:text-sky-600 hover:underline "
+						href="/users"
+					>
+						Открыть список пользователей
+					</Link>
 				</div>
 			</div>
 		</div>
