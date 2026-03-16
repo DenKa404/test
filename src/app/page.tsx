e16bf96c5b58a1d84b08e5e84963ad210b81c3e0
Денис Кабайлов <denis2000kab@gmail.com>
2026-03-16 21:19:50 +0300

    Добавлен заголовок и ссылка на страницу пользователей на домашней странице

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
 	return (
 		<div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-			<h1>Список пользователей</h1>
+			<div className="flex flex-col h-full justify-between">
+				<h1 className='shrink-0 font-semibold text-4xl h-fit'>Начальная страница</h1>
+				<div className='flex-1 h-full text-center'>
+					<Link className='hover:text-sky-600 hover:underline ' href="/users">Открыть список пользователей</Link>
+				</div>
+			</div>
 		</div>
 	);
 }
